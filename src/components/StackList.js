b7d5a10a63dd6bd0e6799a3abe9b01457cb7be57
dexstar <dexstaar@gmail.com>
2017-09-03 15:54:39 +0800

    fin 41

--- src/components/StackList.js
+++ src/components/StackList.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import stacks from "../data/stacks.json";
+import { setStack } from "../actions";
 
 class StackList extends Component {
   render() {
+    console.log("stacklist props", this.props);
+
     return (
       <div>
         {stacks.map(stack => {
           return (
-            <Link to='/stack'>
+            <Link 
+              to="/stack" 
+              key={stack.id} 
+              onClick={() => this.props.setStack(stack)}
+            >
               <h4>{stack.title}</h4>
             </Link>
           );
@@ -18,4 +26,4 @@ class StackList extends Component {
   }
 }
 
-export default StackList;
+export default connect(null, { setStack })(StackList);
